70dcd681dd8a4fb86d8193a6582072bc418d660d
Emiya <emiya@emiya.com.cn>
2024-01-07 17:54:52 +0800

    fix

--- create-simple-package.sh
+++ create-simple-package.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-DIRNAME=$1
+PROJECT_NAME=''
 
-if [ -z "$DIRNAME" ]; then
+if [ -z "$PROJECT_NAME" ]; then
   # GET INPUT DIRECTORY NAME
-  read -p "Enter directory name: " DIRNAME
+  read -p "Enter package name: " PROJECT_NAME
 fi
 
-mkdir "$DIRNAME"
+if [ -e "$PROJECT_NAME" ]; then
+  echo "Directory already exists"
+  exit 1
+fi
+
+mkdir "$PROJECT_NAME"
+
+P0="$0"
+
+CURRENT_PATH=${P0//.bin\//};
 
 # COPY PROJECT DIRECT
-cp -r ./template/* "$DIRNAME"
+cp -r $CURRENT_PATH/template/* "$PROJECT_NAME"
 
 # CHANGE PACKAGE NAME IN PACKAGE.JSON
-sed -i '' "s/emiya-project/$DIRNAME/g" "$DIRNAME/package.json"
+sed -i '' "s/project-name/$PROJECT_NAME/g" "$PROJECT_NAME/package.json"
